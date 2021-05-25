b078f9f54582c5ea5ae0526f1a6926c956e62589
for-ming <forming__@naver.com>
2021-05-25 12:22:29 +0900

    민지작업

---
+++ MyOnlineClass/src/main/java/net/developia/online/dto/InstructorDTO.java
@@ -0,0 +1,12 @@
+package net.developia.online.dto;
+
+import lombok.Data;
+
+@Data
+public class InstructorDTO {
+	private String id;
+	private String name;
+	private String phone;
+	private String introduce;
+	private String image;
+}
